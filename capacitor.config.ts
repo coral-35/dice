56aa64c78650e7a73e45741af34381da05b5cf05
mamama_win <coralbellsgithub@gmail.com>
2025-05-17 22:04:00 +0900

    splashの見切れ修正

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.CoralPlay.dicesimulator',
   appName: 'サイコロシミュレーター',
-  webDir: 'build'
+  webDir: 'build',
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      launchAutoHide: true,
+      backgroundColor: '#ffffff',
+      androidSplashResourceName: 'splash',
+      androidScaleType: 'CENTER_INSIDE',  // CENTER_CROPから変更
+      showSpinner: false,
+      splashFullScreen: true,
+      splashImmersive: true
+    }
+  }
 };
 
 export default config;
