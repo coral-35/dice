fdbc3600abc47f88656bba92a18190ca7fc04009
mamama_win <coralbellsgithub@gmail.com>
2025-05-17 16:40:40 +0900

    capacitor.config.ts を作成, npx cap init

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.CoralPlay.dicesimulator',
+  appName: 'サイコロシミュレーター',
+  webDir: 'build'
+};
+
+export default config;
